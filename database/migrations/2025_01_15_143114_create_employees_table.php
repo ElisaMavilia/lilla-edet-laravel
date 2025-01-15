f7644cd1dfcac825e73e600db748941c00ae1e7d
ElisaMavilia <elisamavilia1@gmail.com>
2025-01-15 17:11:17 +0100

    Added mscm and migrated db

---
+++ database/migrations/2025_01_15_143114_create_employees_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string ('name', 255);
+            $table->string('surname', 255);
+            $table->string('email', 255);
+            $table->string('phone', 255);
+            $table->string('role', 255);
+            $table->bigInteger('salary');
+            $table->date('date_of_employment');
+            $table->string('image')->nullable();
+    
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
