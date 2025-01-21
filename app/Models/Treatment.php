d0c1f3c21020c45339ddcece9fca0342595333f3
ElisaMavilia <elisamavilia1@gmail.com>
2025-01-21 16:48:37 +0100

    Added slug

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -5,12 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Dentist;
+use Illuminate\Support\Str;
 
 class Treatment extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public static function generateSlug($title)
+    {
+        $slug = Str::slug($title, '-');
+        $count = 1;
+        while (Treatment::where('slug', $slug)->first()) {
+            $slug = Str::of($title)->slug('-') . "-{$count}";
+            $count++;
+        }
+        return $slug;
+    }
 
 
     public function dentists() 
