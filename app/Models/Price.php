be798b3955c47fdf57bf8af57d2af128227dbdd6
ElisaMavilia <elisamavilia1@gmail.com>
2025-04-17 14:40:21 +0200

    added priceFormatted function

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -10,4 +10,11 @@ class Price extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    protected $appends = ['price_formatted'];
+
+    public function getPriceFormattedAttribute()
+    {
+        return rtrim(rtrim(number_format($this->price, 2, '.', ''), '0'), '.');
+    }
 }
